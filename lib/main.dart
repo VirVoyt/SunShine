516bf0c293c81a20372d825c901c8a7028944dd0
VirVoyt <nikitavoit23@gmail.com>
2024-07-25 18:38:14 +0300

    added pets and multiplier

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,18 @@ class _MainPageState extends State<MainPage> {
 
   double tapCount = 0;
   bool tapFlag = true;
+
+  //эти параметры будут устанавливать путем покупки чего-либо из магазина
+  bool petBuy1 = false;
+  int petPower1 = 0;
+  bool petBuy2 = true;
+  int petPower2 = 0;
+  bool petBuy3 = true;
+  int petPower3 = 0;
+  bool petBuy4 = true;
+  int petPower4 = 0;
+  int multiplier = 1;
+  int power = 2;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -43,7 +55,7 @@ class _MainPageState extends State<MainPage> {
             onPointerDown: (b){
               setState(() {
                 tapFlag = false;
-                tapCount = tapCount + 1;
+                tapCount = tapCount + 1 * multiplier * power + petPower1 + petPower2 + petPower3 + petPower4;
               });
             },
             onPointerUp: (P){
@@ -60,14 +72,17 @@ class _MainPageState extends State<MainPage> {
                 width: 300,
                 height: 300,
                 alignment: Alignment.center,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(image: AssetImage("assets/sunMain.png" ))
+                decoration:  BoxDecoration(
+                    image: DecorationImage(image: AssetImage(tapFlag ? "assets/sunMain.png" : "assets/sunLight.png" ))
                 ),
                 child: Stack(
                   children: [
+                    Container(alignment: const Alignment(-1.8, 2.5), child: _pet(),),
+                    Container(alignment: const Alignment(1.5, -2), child: _pet2(),),
+                    Container(alignment: const Alignment(-1.5, -2.5), child: _pet3(),),
+                    Container(alignment: const Alignment(1.8, 2), child: _pet4(),),
                     Container(child: _score()),
-                    Container(child: _hitBox())
-                    ],
+                  ],
                 )
               )
             ),
@@ -80,7 +95,6 @@ class _MainPageState extends State<MainPage> {
           image:  DecorationImage( image: AssetImage("assets/1.jpg"))),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
             children: <Widget>[
               Align(
                   child: ElevatedButton(
@@ -92,7 +106,7 @@ class _MainPageState extends State<MainPage> {
                 onPressed: () {
 
                   setState(() {
-                   //переход в магазин
+                   petBuy1 = true;
                   });
                 },
               )),
@@ -116,29 +130,145 @@ class _MainPageState extends State<MainPage> {
   }
   //виджет для вывода текста
   Widget _score(){
-    return  Align(
-        alignment: const Alignment(0.0, -2),
-        child:Text(
-            'Score: $tapCount',
-            textDirection: TextDirection.ltr,   // текст слева направо
-            style: const TextStyle(fontSize: 24) // высота шрифта 24
-        )
+    if(tapCount < 1000) {
+      return Align(
+          alignment: const Alignment(0.0, -2),
+          child: Text(
+              'Score: $tapCount',
+              textDirection: TextDirection.ltr, // текст слева направо
+              style: const TextStyle(fontSize: 24,
+                color: Colors.black,
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 2.0,
+                    color: Color.fromARGB(128, 0, 0, 0),
+                  ),
+                  Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 2.0,
+                    color: Color.fromARGB(128, 0, 0, 0),
+                  ),
+                ],
+              ) // высота шрифта 24
+          )
+      );
+    } else if(tapCount >= 1000 && tapCount < 1000000){
+      double deatCount = tapCount/1000;
+      return Align(
+          alignment: const Alignment(0.0, -2),
+          child: Text(
+              'Score: $deatCount''K',
+              textDirection: TextDirection.ltr, // текст слева направо
+              style: const TextStyle(fontSize: 24,
+                color: Colors.black,
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 2.0,
+                    color: Color.fromARGB(128, 0, 0, 0),
+                  ),
+                  Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 2.0,
+                    color: Color.fromARGB(128, 0, 0, 0),
+                  ),
+                ],
+              ) // высота шрифта 24
+          )
+      );
+    } else if(tapCount >= 1000000){
+      double deatCount = tapCount/1000000;
+      return Align(
+          alignment: const Alignment(0.0, -2),
+          child: Text(
+              'Score: $deatCount''M',
+              textDirection: TextDirection.ltr, // текст слева направо
+              style: const TextStyle(fontSize: 24,
+                color: Colors.black,
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 2.0,
+                    color: Color.fromARGB(128, 0, 0, 0),
+                  ),
+                  Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 2.0,
+                    color: Color.fromARGB(128, 0, 0, 0),
+                  ),
+                ],
+              ) // высота шрифта 24
+          )
+      );
+    }
+    else{ return Container();}
+  }
+// виджеты добавления питомцев
+  Widget _pet(){
+    if (petBuy1 == true){
+      petPower1 = 2;
+    return SizedBox(
+      width: 100,
+      height: 100,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage(tapFlag ? "assets/pet.png" : "assets/petHit.png" ))
+          ),
+        ),
     );
-
+  } else {
+      return Container();
+    }
   }
 
- Widget _hitBox(){ //виджет для эффекта попадания
-    if (tapFlag == false){
-      debugPrint("$tapFlag");
-    return Container(
-      width: 300,
-      height: 300,
-      alignment: Alignment.center,
-      decoration: const BoxDecoration(
-          image: DecorationImage(image: AssetImage("assets/sunLight.png" ))
-      ),
-    );
-    } else{
+  Widget _pet2(){
+    if (petBuy2 == true){
+      petPower2 = 5;
+          return SizedBox(
+        width: 100,
+        height: 100,
+        child: Container(
+          decoration:  BoxDecoration(
+              image: DecorationImage(image: AssetImage(tapFlag ?  "assets/бабочка.png" : "assets/бабочкаHit.png"))
+          ),
+        ),
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  Widget _pet3(){
+    if (petBuy2 == true){
+      petPower3 = 7;
+      return SizedBox(
+        width: 100,
+        height: 100,
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage(tapFlag ? "assets/облако.png" : "assets/облакоHit.png"))
+          ),
+        ),
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  Widget _pet4(){
+    if (petBuy2 == true){
+      petPower4 = 9;
+      return SizedBox(
+        width: 100,
+        height: 100,
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage(tapFlag ? "assets/жук.png" : "assets/жукHit.png" ))
+          ),
+        ),
+      );
+    } else {
       return Container();
     }
   }
